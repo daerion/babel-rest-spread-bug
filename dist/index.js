fdea1750643421f713d7bab4eb92eea424f59be1
Michael Smesnik <ms@tailored-apps.com>
2017-04-13 16:14:07 +0200

    add more example code, rename variables for clarity

--- dist/index.js
+++ dist/index.js
@@ -1,16 +1,45 @@
 function _objectWithoutProperties(obj, keys) { var target = {}; for (var i in obj) { if (keys.indexOf(i) >= 0) continue; if (!Object.prototype.hasOwnProperty.call(obj, i)) continue; target[i] = obj[i]; } return target; }
 
-const data = {
-  foo: {
-    bar: {
-      baz: 'baz',
-      qux: 'qux'
+const working = {
+  nested: {
+    one: 'one',
+    two: 'two'
+  }
+};
+
+const { nested: { one } } = working,
+      more = _objectWithoutProperties(working.nested, ['one']);
+
+console.log('one: %s', one);
+console.dir(more);
+const defunct = {
+  outer: {
+    inner: {
+      three: 'three',
+      four: 'four'
     }
   }
 };
 
-const { foo: { bar: { baz } } } = data,
-      other = _objectWithoutProperties(data.bar.foo, ['baz']);
+const { outer: { inner: { three } } } = defunct,
+      other = _objectWithoutProperties(defunct.inner.outer, ['three']);
 
-console.log('baz: %s', baz);
+console.log('three: %s', three);
 console.dir(other);
+
+const defunct2 = {
+  outer: {
+    inner: {
+      innermost: {
+        five: 'five',
+        six: 'six'
+      }
+    }
+  }
+};
+
+const { outer: { inner: { innermost: { five } } } } = defunct2,
+      props = _objectWithoutProperties(defunct2.innermost.inner.outer, ['five']);
+
+console.log('five: %s', five);
+console.dir(props);
--- src/index.js
+++ src/index.js
@@ -1,13 +1,43 @@
-const data = {
-  foo: {
-    bar: {
-      baz: 'baz',
-      qux: 'qux'
+// Nesting only one level works
+const working = {
+  nested: {
+    one: 'one',
+    two: 'two'
+  }
+}
+
+const { nested: { one, ...more } } = working
+
+console.log('one: %s', one)  // 'one'
+console.dir(more) // { two: 'two' }
+
+// Nesting two or more levels does not work
+const defunct = {
+  outer: {
+    inner: {
+      three: 'three',
+      four: 'four'
     }
   }
 }
 
-const { foo: { bar: { baz, ...other } } } = data
+const { outer: { inner: { three, ...other } } } = defunct
 
-console.log('baz: %s', baz)
-console.dir(other)
+console.log('three: %s', three) // 'three'
+console.dir(other) // { four: 'four' }
+
+const defunct2 = {
+  outer: {
+    inner: {
+      innermost: {
+        five: 'five',
+        six: 'six'
+      }
+    }
+  }
+}
+
+const { outer: { inner: { innermost: { five, ...props } } } } = defunct2
+
+console.log('five: %s', five) // 'five'
+console.dir(props) // { six: 'six' }
